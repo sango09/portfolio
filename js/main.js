f8a8dae44b8f26800aa4a409de157f1b3aea7505
Santiago <santiagofl123@gmail.com>
2020-07-24 15:42:23 -0500

    Create navbar with profile and menu

---
+++ js/main.js
@@ -0,0 +1,15 @@
+openNav = () => {
+    const toggle = document.getElementById("sidebar")
+    const icon = document.getElementById("icon")
+    if (toggle.classList.contains("toggled") && icon.classList.contains("fa-bars")) {
+        toggle.classList.remove("toggled")
+        icon.classList.remove("fa-bars")
+        icon.classList.add("fa-times")
+    } else {
+        toggle.classList.add("toggled")
+        icon.classList.remove("fa-times")
+        icon.classList.add("fa-bars")
+
+
+    }
+}
