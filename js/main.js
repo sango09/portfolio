27274ed038c4694b7e6232749adc638657f8da71
Santiago Gomez Florez <santiagofl123@gmail.com>
2020-07-26 18:15:03 -0500

    New styles for about and skill sections

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 openNav = () => {
+    'use strict'
     const toggle = document.getElementById("sidebar")
     const icon = document.getElementById("icon")
+
     if (toggle.classList.contains("toggled") && icon.classList.contains("fa-bars")) {
         toggle.classList.remove("toggled")
         icon.classList.remove("fa-bars")
         icon.classList.add("fa-times")
+
     } else {
         toggle.classList.add("toggled")
         icon.classList.remove("fa-times")
         icon.classList.add("fa-bars")
-
-
     }
 }
