9e9cb72f36e143ab0d340fbe72da8ff398b12795
Santiago Gomez Florez <santiagofl123@gmail.com>
2020-07-27 19:19:21 -0500

    create class for openNav

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
-openNav = () => {
-    'use strict'
-    const toggle = document.getElementById("sidebar")
-    const icon = document.getElementById("icon")
+class Main {
 
-    if (toggle.classList.contains("toggled") && icon.classList.contains("fa-bars")) {
-        toggle.classList.remove("toggled")
-        icon.classList.remove("fa-bars")
-        icon.classList.add("fa-times")
-
-    } else {
-        toggle.classList.add("toggled")
-        icon.classList.remove("fa-times")
-        icon.classList.add("fa-bars")
+    constructor() {
+        this.openNav()
     }
+
+    openNav() {
+        const toggle = document.getElementById("sidebar")
+        const icon = document.getElementById("icon")
+
+        if (toggle.classList.contains("toggled") && icon.classList.contains("fa-bars")) {
+            toggle.classList.remove("toggled")
+            icon.classList.remove("fa-bars")
+            icon.classList.add("fa-times")
+
+        } else {
+            toggle.classList.add("toggled")
+            icon.classList.remove("fa-times")
+            icon.classList.add("fa-bars")
+        }
+    }
+
+
 }
+
+function startMain() {
+    window.main = new Main()
+}
+
