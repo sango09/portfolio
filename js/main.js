158c4ad1d5dbbb17a136bc320bee569f163b3e5b
Santiago Gomez Florez <santiagofl123@gmail.com>
2020-07-31 20:37:17 -0500

    final changes in main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,23 @@
-class Main {
+openNav = () => {
+    const toggle = document.getElementById("sidebar")
+    const icon = document.getElementById("icon")
 
-    constructor() {
-        this.openNav()
+    if (toggle.classList.contains("toggled") && icon.classList.contains("fa-bars")) {
+        toggle.classList.remove("toggled")
+        icon.classList.remove("fa-bars")
+        icon.classList.add("fa-times")
+
+    } else {
+        toggle.classList.add("toggled")
+        icon.classList.remove("fa-times")
+        icon.classList.add("fa-bars")
     }
-
-    openNav() {
-        const toggle = document.getElementById("sidebar")
-        const icon = document.getElementById("icon")
-
-        if (toggle.classList.contains("toggled") && icon.classList.contains("fa-bars")) {
-            toggle.classList.remove("toggled")
-            icon.classList.remove("fa-bars")
-            icon.classList.add("fa-times")
-
-        } else {
-            toggle.classList.add("toggled")
-            icon.classList.remove("fa-times")
-            icon.classList.add("fa-bars")
-        }
-    }
-
-
 }
 
-function startMain() {
-    window.main = new Main()
-}
-
+//Active Class
+$(document).ready(function () {
+    $('ul li').click(function () {
+        $('li').removeClass("active");
+        $(this).addClass("active");
+    });
+});
